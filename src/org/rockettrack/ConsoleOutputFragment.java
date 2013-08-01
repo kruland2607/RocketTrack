a0dea2ba9adbce27e5d64b91a6ca198b7bfb0094
kruland2607 <kruland2607@github.com>
2013-07-31 19:57:04 -0500

    Have ConsoleOutputFragment extend RocketTrackBaseFragment so the gps is always on.

--- src/org/rockettrack/ConsoleOutputFragment.java
+++ src/org/rockettrack/ConsoleOutputFragment.java
@@ -4,15 +4,27 @@ import org.rockettrack.views.ConsoleOutputView;
 
 import android.database.DataSetObserver;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class ConsoleOutputFragment extends Fragment {
+public class ConsoleOutputFragment extends RocketTrackBaseFragment {
 
 	private DataSetObserver mObserver = null;
 	
+	@Override
+	protected void onRocketLocationChange() {
+	}
+
+	@Override
+	protected void onCompassChange() {
+	}
+
+	@Override
+	protected void onMyLocationChange() {
+		
+	}
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
